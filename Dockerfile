e02a825f6dcc90e8c26fed92185b0edea30bdc11
devJuban <jubanreal500@gmail.com>
2026-01-02 10:41:05 +0000

    [preview] 24/7 beta + improved image

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,4 @@
-#FROM ubuntu:latest
-#FROM rust:latest
 FROM eclipse-temurin:21-jdk
 COPY . .
-
-#RUN apt-get update && apt-get upgrade -y
-#RUN apt-get install openjdk-21-jre -y
-#RUN apt-get install rustc -y
-#RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-#RUN apt-get install cargo -y
-#RUN cargo install bore-cli
-
 RUN chmod +x main.sh
-#RUN cargo install bore-cli
-#RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 CMD ["./main.sh"]
--- main.sh
+++ main.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 cd velocity
 
+# Change these values if you are self hosting
+# Set the render variable to "self-host"
+
+MOTD=$MOTD
+MAXPLAYERS=$MAXPLAYERS
+SERVER=$SERVER
+RENDER=$RENDER
+IMAGE=$IMAGE
+SECRET=$SECRET
+
 # Check for valid Config Info
 
 # Required
+
+# move down to optional (below)
 
 if [ "$MAXPLAYERS" = "true" ] || [ "$MAXPLAYERS" = "" ]; then
     echo "Please configure your MAXPLAYERS environment variable."
     exit 1
 fi
+
+# dont move $server down to optional
 
 if [ "$SERVER" = "true" ] || [ "$SERVER" = "" ]; then
     echo "Please configure your SERVER environment variable."
@@ -29,14 +43,7 @@ if [ "$IMAGE" = "true" ] || [ "$IMAGE" = "" ]; then
     echo "No IMAGE environment variable configured, leaving blank."
 fi
 
-if [ "$PLAY_SECRET" = "true" ] || [ "$PLAY_SECRET" = "" ]; then
-    echo "No PLAY_SECRET environment variable configured, this is normal if this is the first time you start the service."
-    PLAY_SECRET=""
-fi
-
 # Display Config Info
-
-echo $PLAY_SECRET
 
 echo "Your MOTD (eagler only): $MOTD"
 echo "Your Max Players (eagler only): $MAXPLAYERS"
@@ -53,29 +60,19 @@ sed -i 's/${SERVER}/'"$SERVER"'/g' velocity.toml
 cd plugins/eaglerxserver
 sed -i 's/${MOTD}/'"$MOTD"'/g' listeners.toml
 cd ../velocit
-sed -i 's/${PLAY_SECRET}/'"$PLAY_SECRET"'/g' config.toml
 
 cd ../..
 
 # Set up server-icon.png
 
-mkdir images && cd images
-wget $IMAGE
-
-for file in *
-do
-    mv $file "../server-icon.png"
-done
-
-cd ..
-
-rmdir images
+wget -O server-icon.png $IMAGE
 
 # Set up 24/7 web service
 
-while true; do sleep 120; curl $RENDER ; done &
-
+#while true; do sleep 120; curl $RENDER ; done &
+# 24/7 beta
+while true; do sleep 120; curl 0.0.0.0 ; done &
 # Start Velocity
 
 echo "Starting Velocity | For Render"
-java -Xmx512M -Xms512M -jar velocity.jar
+java -Xms512M -Xmx512M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity.jar
