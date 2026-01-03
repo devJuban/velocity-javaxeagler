d229bd6599873428296cd650dde305a180f3f07c
devJuban <jubanreal500@gmail.com>
2026-01-03 12:19:58 +0000

    added playit.gg tunnel (use tcp not minecraft java)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
 FROM eclipse-temurin:21-jdk
+RUN apt-get update
+RUN curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
+RUN echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
+RUN apt-get update
+RUN apt-get install playit -y
 COPY . .
 RUN chmod +x main.sh
 CMD ["./main.sh"]
--- main.sh
+++ main.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 cd velocity
 
-# Check for valid Config Info
+# playit agent
+playit start &
+echo "Please claim the tunnel above, you have 5 seconds before the script continues..."
+sleep 5
+
+# Check for valid Config Info & Display Config Info
 
 ## Required
 
 if [ "$SERVER" = "true" ] || [ "$SERVER" = "" ]; then
     echo "Please configure your SERVER environment variable."
     exit 1
+else
+    echo "Your IP: $SERVER"
 fi
 
 ## Optional
@@ -15,34 +22,41 @@ fi
 if [ "$MOTD" = "true" ] || [ "$MOTD" = "" ]; then
     echo "No MOTD environment variable configured, setting to default."
     MOTD="      \&bThis server is being hosted by \&4\&l\&nv-jXe!\&r\n              \&e\&l\&ntinyurl.com/mvk7f4xh"
+else
+    echo "Your MOTD: $MOTD"
 fi
 
 if [ "$MAXPLAYERS" = "true" ] || [ "$MAXPLAYERS" = "" ]; then
     echo "No MAXPLAYERS environment variable configured, setting to default (100)."
     MAXPLAYERS=100
+else
+    echo "Your Max Players: $MAXPLAYERS"
 fi
 
 if [ "$RENDER" = "true" ] || [ "$RENDER" = "" ]; then
     echo "No RENDER environment variable configured, this web service will NOT be 24/7."
+else
+    echo "Your Render IP: $RENDER"
+
+    # 24/7
+    while true; do sleep 120; curl $RENDER ; done &
 fi
 
 if [ "$IMAGE" = "true" ] || [ "$IMAGE" = "" ]; then
     echo "No IMAGE environment variable configured, leaving blank."
+else
+    echo "Your \"server-icon.png\" will be pulled from: $IMAGE"
 fi
 
 if [ "$SECRET" = "true" ] || [ "$SECRET" = "" ]; then
     echo "No SECRET environment variable configured, generating a random one."
     SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1 )
+    echo "Your Secret is: $SECRET"
+else
+    echo "Your Secret is: $SECRET"
 fi
 
-# Display Config Info
-
-echo "Your MOTD (eagler only): $MOTD"
-echo "Your Max Players (eagler only): $MAXPLAYERS"
-echo "Your IP: $SERVER"
-echo "Your Render IP: $RENDER"
-echo "Your \"server-icon.png\" will be pulled from: $IMAGE"
-echo "Your Secret is: $SECRET"
+# Set config
 
 sed -i 's|${SECRET}|'"$SECRET"'|g' forwarding.secret
 sed -i 's|${MOTD}|'"$MOTD"'|g' velocity.toml
@@ -56,10 +70,6 @@ cd ../..
 # Set up server-icon.png
 
 wget -O server-icon.png $IMAGE
-
-# Set up 24/7 web service
-
-while true; do sleep 120; curl $RENDER ; done &
 
 # Start Velocity
 
