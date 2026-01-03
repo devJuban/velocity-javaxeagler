1be0d93d5e50f73c2d77295f9034d3af09427ab9
devJuban <jubanreal500@gmail.com>
2026-01-03 18:37:55 +0000

    testing velocity motd

--- main.sh
+++ main.sh
@@ -12,7 +12,7 @@ convert_motd_velocity() {
     converted_color_codes=(["&0"]="<black>" ["&1"]="<dark_blue>" ["&2"]="<dark_green>" ["&3"]="<dark_aqua>" ["&4"]="<dark_red>" ["&5"]="<dark_purple>" ["&6"]="<gold>" ["&7"]="<gray>" ["&8"]="<dark_gray>" ["&9"]="<blue>" ["&a"]="<green>" ["&b"]="<aqua>"["&c"]="<red>" ["&d"]="<light_purple>" ["&e"]="<yellow>" ["&f"]="<white>" ["&l"]="<bold>" ["&m"]="<strikethrough>" ["&n"]="<underline>" ["&o"]="<italic>" ["&k"]="<obfuscated>" ["&r"]="<reset>")
 
     for code in "${converted_color_codes[@]}"; do
-        1="${1//$code/"${converted_color_codes[$code]}"}"
+        1="${1//$code/"converted_color_codes[$code]"}"
     done
 
     echo "$1"
