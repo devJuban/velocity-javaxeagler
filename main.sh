1610a66ef646e4b7364ccd24d5c153429d4fc544
devJuban <jubanreal500@gmail.com>
2026-01-03 18:00:16 +0000

    testing velocity motd

--- main.sh
+++ main.sh
@@ -7,6 +7,41 @@ convert_motd() {
     echo "${1//\&/"\\&"}"
 }
 
+convert_motd_velocity() {
+    converted_color_codes=(
+        "&0"="<black>"
+        "&1"="<dark_blue>"
+        "&2"="<dark_green>"
+        "&3"="<dark_aqua>"
+        "&4"="<dark_red>"
+        "&5"="<dark_purple>"
+        "&6"="<gold>"
+        "&7"="<gray>"
+        "&8"="<dark_gray>"
+        "&9"="<blue>"
+        "&a"="<green>"
+        "&b"="<aqua>"
+        "&c"="<red>"
+        "&d"="<light_purple>"
+        "&e"="<yellow>"
+        "&f"="<white>"
+        "&l"="<bold>"
+        "&m"="<strikethrough>"
+        "&n"="<underline>"
+        "&o"="<italic>"
+        "&k"="<obfuscated>"
+        "&r"="<reset>"
+    )
+
+    new_motd="$1"
+
+    for code in "${converted_color_codes[@]}"; do
+        new_motd="${new_motd//$code/"${converted_color_codes[$code]}"}"
+    done
+
+    echo "$new_motd"
+}
+
 # Check for valid Config Info & Display Config Info
 
 ## Required
